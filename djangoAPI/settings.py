548333be7e5e7f6bd3257d67f417c809a3972a66
Soraya Codo <soraya.codo@epitech.eu>
2023-02-09 20:44:37 +0100

    Initial commit

--- djangoAPI/settings.py
+++ djangoAPI/settings.py
@@ -113,7 +113,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoAPI.wsgi.application'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
