ec5b053605056a712c702099fab1b6eeeb870548
Soraya Codo <soraya.codo@epitech.eu>
2023-02-09 20:57:30 +0100

    Initial commit

--- djangoAPI/wsgi.py
+++ djangoAPI/wsgi.py
@@ -10,17 +10,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoAPI.settings')
 
-application = get_wsgi_application()
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoAPI.settings")
 # Remember to change coffe.settings to your_project_name.settings
 
 application = get_wsgi_application()
