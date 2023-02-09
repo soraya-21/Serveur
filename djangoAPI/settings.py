7a3f9374a240f26e05f0f00f0c4f250a4fc4434b
Soraya Codo <soraya.codo@epitech.eu>
2023-02-09 21:42:19 +0100

    Initial commit

--- djangoAPI/settings.py
+++ djangoAPI/settings.py
@@ -182,6 +182,7 @@ STATICFILES_DIRS = (
 os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DISABLE_COLLECTSTATIC=1
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
