bac40bff0b3a99ffee1c0af3bfb17b073c296ac1
Soraya Codo <soraya.codo@epitech.eu>
2023-02-14 17:44:35 +0100

    changed database to mongo

--- djangoAPI/settings.py
+++ djangoAPI/settings.py
@@ -117,23 +117,23 @@ WSGI_APPLICATION = 'djangoAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'djongo',
-#         'NAME' : "AREA",
-#         'CLIENT': {
-#             "host": "mongodb+srv://areadatabase:areadatabase@cluster0.lpn6dup.mongodb.net/?retryWrites=true&w=majority",
-#             "authMechanism": "SCRAM-SHA-1"
-#         }
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME' : "AREA",
+        'CLIENT': {
+            "host": "mongodb+srv://areadatabase:areadatabase@cluster0.lpn6dup.mongodb.net/?retryWrites=true&w=majority",
+            "authMechanism": "SCRAM-SHA-1"
+        }
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME':os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 REST_FRAMEWORK = {
